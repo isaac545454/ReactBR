07128f5d48c3a764da84aee077a8985188592712
Isaac1221 <isaac.newton1221@gmail.com>
2023-11-09 09:30:48 -0300

    feat: add page

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './style/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div></div>
   </React.StrictMode>,
 )
