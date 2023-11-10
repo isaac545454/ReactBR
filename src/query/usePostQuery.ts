84165b11740cc50842c8aef7c3bca2fedee5bdd3
Isaac1221 <isaac.newton1221@gmail.com>
2023-11-10 15:22:37 -0300

    fix: fea

--- src/query/usePostQuery.ts
+++ src/query/usePostQuery.ts
@@ -1,10 +1,1 @@
-import { useHttpQuery } from "../hooks/useHttpQuery"
-
-export const usePostQuery = () => {
-    return useHttpQuery({
-        queryKey: ['getPost'],
-        HttpService: {
-           
-        }
-    })
-}
+ 
