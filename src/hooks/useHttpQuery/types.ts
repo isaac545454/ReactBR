f3203c83eaef363a4c77123d767b5873732f2772
Isaac1221 <isaac.newton1221@gmail.com>
2023-11-10 15:20:58 -0300

    feat: add

--- src/hooks/useHttpQuery/types.ts
+++
@@ -1,8 +0,0 @@
-import { QueryKey, UseQueryOptions } from '@tanstack/react-query'
-import { HttpProps } from '../../types/HttpProps'
-
-export interface IGetMutation<TData, TError = unknown> {
-	queryKey: QueryKey
-	options?: UseQueryOptions<TData, TError>
-	HttpService: HttpProps
-}
--- src/hooks/useNotification/types.ts
+++
@@ -1,5 +0,0 @@
-export type NotificationFunctions = {
-	error: (msg: string) => void
-	warn: (msg: string) => void
-	success: (msg: string) => void
-}
