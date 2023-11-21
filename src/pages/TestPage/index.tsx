22c9bcc5f6133a89ca7289525be36c47a6cff0fa
Isaac1221 <isaac.newton1221@gmail.com>
2023-11-21 19:05:23 -0300

    feat: test

--- src/pages/TestPage/index.tsx
+++ src/pages/TestPage/index.tsx
@@ -1,11 +1,16 @@
+import { useTestQuey } from '@/query/useTestQuey'
 import { useTestPage } from './hook'
 import { TestPageProps } from './types'
+import { TestComponent } from '@/components/TestComponent'
 
 export const TestPage = ({ }: TestPageProps) => {
   const { } = useTestPage()
+  const {data,error } = useTestQuey()
   
   return (
-    <div></div>
+    <div>
+      <TestComponent />
+    </div>
   );
 }
 
